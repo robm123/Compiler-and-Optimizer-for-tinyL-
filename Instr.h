8930136b6d49faf5fbdb40ab2fb82701db54edeb
Rob <Roberto.Merino@Rutgers.edu>
2016-05-17 01:48:52 -0400

    Update Instr.h

--- Instr.h
+++ Instr.h
@@ -2,8 +2,7 @@
  *********************************************
  *  314 Principles of Programming Languages  *
  *  Fall 2015                                *
- *  Author: Ulrich Kremer                    *
- *  Student Version                          *
+ *  Roberto Merino	                     *
   *********************************************
  */
 
@@ -21,7 +20,7 @@ struct InstructionInfo {
 	int field3;
 	Instruction *prev;	/* previous instruction */
 	Instruction *next;	/* next instruction */
-	char critical;   // 'c' == crucial ........'n' == not crucial instruction
+	char critical;         // 'c' == crucial ........'n' == not crucial instruction
 };
 
 #endif
